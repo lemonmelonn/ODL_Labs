e5219494c14483832c5682df1443bd9eb6ecf1bd
Devan <devanasokan@gmail.com>
2026-07-27 12:04:46 +0800

    27/7 - morning work

--- Assignment/combine.py
+++ Assignment/combine.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-df1 = pd.read_csv("C:\\Users\\User\\ODL_Labs\\Assignment\\customer_churn_dataset-testing-master.csv")
-df2 = pd.read_csv("C:\\Users\\User\\ODL_Labs\\Assignment\\customer_churn_dataset-training-master.csv")
+df1 = pd.read_csv("C:\\Users\\User\\ODL_Labs\\Assignment\\data\\customer_churn_dataset-testing-master.csv")
+df2 = pd.read_csv("C:\\Users\\User\\ODL_Labs\\Assignment\\data\\customer_churn_dataset-training-master.csv")
 
 # Combine the two dataframes into a single dataframe
 df = pd.concat([df1, df2], ignore_index=True)
@@ -9,4 +9,4 @@ df = pd.concat([df1, df2], ignore_index=True)
 print("Combined dataframe shape:", df.shape)
 
 # Save the combined dataframe to a new CSV file
-df.to_csv("C:\\Users\\User\\ODL_Labs\\Assignment\\full_churn_dataset.csv", index=False)
+df.to_csv("C:\\Users\\User\\ODL_Labs\\Assignment\\data\\full_churn_dataset.csv", index=False)
